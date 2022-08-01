2ddcd0850701a91a7991aaede31240cf263f3096
Lucas Dias <lucasgdias@gmail.com>
2022-08-01 13:16:14 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace TestMetalamaParameters;
+
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Calling a first time.");
+        Add<GenericType>(1, 1);
+        Console.WriteLine("Calling a second time.");
+        Add<GenericType>(1, 1);
+        Console.WriteLine("Completed.");
+    }
+
+[Cache]
+    public static int Add<T>(int a, int b, bool skipCache = false)
+    {
+        Console.WriteLine("Thinking...");
+
+        return a + b;
+    }
+}
+
+public class GenericType { }
